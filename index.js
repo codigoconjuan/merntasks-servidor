1d2410572fa8f1c7bb6efa814169d9b535278254
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-20 17:29:14 -0600

    configurando express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('desde index.js')
+const express = require('express');
+
+// crear el servidor
+const app = express();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
