b22d96191c641db083159725173374bb62e78d1b
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 12:18:43 -0600

    modelo de usuarios

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // Conectar a la base de datos
 conectarDB();
 
+// Habilitar express.json
+app.use( express.json({ extended: true }));
+
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
---
+++ models/Usuario.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UsuariosSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true, 
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    registro: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Usuario', UsuariosSchema);
