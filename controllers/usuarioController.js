cb76282cd24aaff24c0575f07405bc266b13b0a5
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 13:18:22 -0600

    firmando JWT

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,9 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async (req, res) => {
-
 
     // revisar si hay errores
     const errores = validationResult(req);
@@ -33,8 +33,24 @@ exports.crearUsuario = async (req, res) => {
         // guardar usuario
         await usuario.save();
 
-        // Mensaje de confirmación
-        res.json({ msg: 'Usuario creado correctamente' });
+        // Crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmación
+            res.json({ token  });
+        });
+
+
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
