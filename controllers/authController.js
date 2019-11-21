87c5e1975d49bf0ec81f2798c30c1cec2722b0be
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 13:34:51 -0600

    auth con jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,5 +10,40 @@ exports.autenticarUsuario = async (req, res) => {
         return res.status(400).json({errores: errores.array() })
     }
 
-    
+    // extraer el email y password
+    const { email, password } = req.body;
+
+    try {
+        // Revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({ email });
+        if(!usuario) {
+            return res.status(400).json({msg: 'El usuario no existe'});
+        }
+
+        // Revisar el password
+        const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        if(!passCorrecto) {
+            return res.status(400).json({msg: 'Password Incorrecto' })
+        }
+
+        // Si todo es correcto Crear y firmar el JWT
+         const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmación
+            res.json({ token  });
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const { check } = require('express-validator');
 const authController = require('../controllers/authController');
 
 // Crea un usuario
-// api/usuarios
+// api/auth
 router.post('/', 
     [
         check('email', 'Agrega un email válido').isEmail(),
