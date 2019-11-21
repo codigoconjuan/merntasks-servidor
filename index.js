e4aac4734f5bdd0b7bc494b4f248c305d85befb2
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-20 18:44:57 -0600

    conexión DB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // crear el servidor
 const app = express();
+
+// Conectar a la base de datos
+conectarDB();
 
 // puerto de la app
 const PORT = process.env.PORT || 4000;
