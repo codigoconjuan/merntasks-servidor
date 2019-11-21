94f9234de40084c04784200b063660e7b382bd1a
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 14:35:08 -0600

    validar proyectos

--- controllers/proyectocontroller.js
+++ controllers/proyectocontroller.js
@@ -1,6 +1,14 @@
 const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
 
 exports.crearProyecto = async (req, res) => {
+
+    // Revisar si hay errores
+    const errores = validationResult(req);
+    if( !errores.isEmpty() ) {
+        return res.status(400).json({errores: errores.array() })
+    }
+
 
     try {
         // Crear un nuevo proyecto
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 // Crea proyectos
 // api/proyectos
 router.post('/', 
     auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatoio').not().isEmpty()
+    ],
     proyectoController.crearProyecto
 );
 
