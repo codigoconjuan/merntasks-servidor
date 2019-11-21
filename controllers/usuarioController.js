48f0690e9de5f98a7603394891e8d29d1095693e
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 12:42:08 -0600

    password hasheados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
 
@@ -14,9 +15,12 @@ exports.crearUsuario = async (req, res) => {
             return res.status(400).json({ msg: 'El usuario ya existe' });
         }
 
-
         // crea el nuevo usuario
         usuario = new Usuario(req.body);
+
+        // Hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt );
 
         // guardar usuario
         await usuario.save();
