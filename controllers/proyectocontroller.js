e5fadb221a54cd56aac8cff20ba00dd579743ae8
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 14:14:02 -0600

    controller para poyectos

--- controllers/proyectocontroller.js
+++ controllers/proyectocontroller.js
@@ -1,4 +1,15 @@
+const Proyecto = require('../models/Proyecto');
 
 exports.crearProyecto = async (req, res) => {
-    
+
+    try {
+        // Crear un nuevo proyecto
+        const proyecto = new Proyecto(req.body);
+        proyecto.save();
+        res.json(proyecto);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const proyectoController = require('../controllers/proyectocontroller');
+const proyectoController = require('../controllers/proyectoController');
 
 // Crea proyectos
 // api/proyectos
