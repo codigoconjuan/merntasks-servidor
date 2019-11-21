0ddd42084caf5be2e82051a329f5fc2be1f41032
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-20 19:30:23 -0600

    Creando controller de usuarios

---
+++ controllers/usuarioController.js
@@ -0,0 +1,5 @@
+
+
+exports.crearUsuario = (req, res) => {
+    console.log(req.body);
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,12 @@
 // Rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 // Crea un usuario
 // api/usuarios
-router.post('/', () => {
-
-});
+router.post('/', 
+    usuarioController.crearUsuario
+);
 
 module.exports = router;
