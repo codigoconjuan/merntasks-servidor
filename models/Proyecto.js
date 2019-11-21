fdd2d33dad9734efd5bbdf6cfd29770f0084456c
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 13:39:45 -0600

    modelo de proyectos

---
+++ models/Proyecto.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ProyectoSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    creador: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Usuario'
+    },
+    creado: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Proyecto', ProyectoSchema);
