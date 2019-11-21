f7f239a5d18d97d2e94b159d26a519bf6d294a8b
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-20 19:06:51 -0600

    Creando routing de usuarios

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ conectarDB();
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 // arrancar la app
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
---
+++ routes/usuarios.js
@@ -0,0 +1,11 @@
+// Rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+
+// Crea un usuario
+// api/usuarios
+router.post('/', () => {
+
+});
+
+module.exports = router;
