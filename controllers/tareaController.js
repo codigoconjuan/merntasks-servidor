df9ea89da4dd06409b472e120cfb79561b16dae9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 17:32:25 -0600

    filtrando tareas por proyecto

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -38,3 +38,30 @@ exports.crearTarea = async (req, res) => {
     }
 
 }
+
+// Obtiene las tareas por proyecto
+exports.obtenerTareas = async (req, res) => {
+
+        try {
+            // Extraer el proyecto y comprobar si existe
+            const { proyecto } = req.body;
+
+            const existeProyecto = await Proyecto.findById(proyecto);
+            if(!existeProyecto) {
+                return res.status(404).json({msg: 'Proyecto no encontrado'})
+            }
+
+            // Revisar si el proyecto actual pertenece al usuario autenticado
+            if(existeProyecto.creador.toString() !== req.usuario.id ) {
+                return res.status(401).json({msg: 'No Autorizado'});
+            }
+
+            // Obtener las tareas por proyecto
+            const tareas = await Tarea.find({ proyecto });
+            res.json({ tareas });
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Hubo un error');
+        }
+}
--- routes/tareas.js
+++ routes/tareas.js
@@ -15,4 +15,10 @@ router.post('/',
     tareaController.crearTarea
 );
 
+// Obtener las tareas por proyecto
+router.get('/',
+    auth,
+    tareaController.obtenerTareas
+);
+
 module.exports = router;
