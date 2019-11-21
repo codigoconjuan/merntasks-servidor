16b00bf196e5fc3d4474f3117e2dae8eb4adcc9a
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 12:35:06 -0600

    revisando que no haya usuarios duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,19 @@
 const Usuario = require('../models/Usuario');
 
 exports.crearUsuario = async (req, res) => {
-    
+
+    // extraer email y password
+    const { email, password } = req.body;
+
+
     try {
-        let usuario;
+        // Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe' });
+        }
+
 
         // crea el nuevo usuario
         usuario = new Usuario(req.body);
@@ -12,7 +22,7 @@ exports.crearUsuario = async (req, res) => {
         await usuario.save();
 
         // Mensaje de confirmación
-        res.send('Usuario creado correctamente')
+        res.json({ msg: 'Usuario creado correctamente' });
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
