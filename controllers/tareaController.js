48421b7cef3d03b59e8847e15fcdd48534956223
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-21 18:06:48 -0600

    eliminar tareas

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -95,7 +95,39 @@ exports.actualizarTarea = async (req, res ) => {
         tarea = await Tarea.findOneAndUpdate({_id : req.params.id }, nuevaTarea, { new: true } );
 
         res.json({ tarea });
-        
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
+    }
+}
+
+
+// Elimina una tarea
+exports.eliminarTarea = async (req, res) => {
+    try {
+        // Extraer el proyecto y comprobar si existe
+        const { proyecto  } = req.body;
+
+        // Si la tarea existe o no
+        let tarea = await Tarea.findById(req.params.id);
+
+        if(!tarea) {
+            return res.status(404).json({msg: 'No existe esa tarea'});
+        }
+
+        // extraer proyecto
+        const existeProyecto = await Proyecto.findById(proyecto);
+
+        // Revisar si el proyecto actual pertenece al usuario autenticado
+        if(existeProyecto.creador.toString() !== req.usuario.id ) {
+            return res.status(401).json({msg: 'No Autorizado'});
+        }
+
+        // Eliminar
+        await Tarea.findOneAndRemove({_id: req.params.id});
+        res.json({msg: 'Tarea Eliminada'})
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error')
--- routes/tareas.js
+++ routes/tareas.js
@@ -27,4 +27,10 @@ router.put('/:id',
     tareaController.actualizarTarea
 );
 
+// Eliminar tarea
+router.delete('/:id', 
+    auth,
+    tareaController.eliminarTarea
+);
+
 module.exports = router;
