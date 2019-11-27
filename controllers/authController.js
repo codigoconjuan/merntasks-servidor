7b0f34523def368c6831dbc8e9f4d979a7113aaa
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-26 19:30:51 -0600

    autenticar usuario y obtener usuario autenticado

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,7 +52,7 @@ exports.autenticarUsuario = async (req, res) => {
 // Obtiene que usuario esta autenticado
 exports.usuarioAutenticado = async (req, res) => {
     try {
-        const usuario = await Usuario.findById(req.usuario.id);
+        const usuario = await Usuario.findById(req.usuario.id).select('-password');
         res.json({usuario});
     } catch (error) {
         console.log(error);
