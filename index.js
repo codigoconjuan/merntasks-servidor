9fd7e736e384f4f6fde9bc19a6b4bf2cfda772b0
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-26 18:28:06 -0600

    cors y registro de usuario

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 // crear el servidor
 const app = express();
 
 // Conectar a la base de datos
 conectarDB();
+
+// habilitar cors
+app.use(cors());
 
 // Habilitar express.json
 app.use( express.json({ extended: true }));
