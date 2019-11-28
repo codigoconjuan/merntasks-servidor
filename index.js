8b646c24e9cd2870edefc27e9703c450d62ee16c
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-28 15:34:25 -0600

    puerto

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use( express.json({ extended: true }));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
