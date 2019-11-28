682782e646f6366b64e40c5795ea018d9ccc25eb
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-28 14:15:30 -0600

    actualizar tareas

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -73,6 +73,7 @@ exports.actualizarTarea = async (req, res ) => {
         // Extraer el proyecto y comprobar si existe
         const { proyecto, nombre, estado } = req.body;
 
+
         // Si la tarea existe o no
         let tarea = await Tarea.findById(req.params.id);
 
@@ -89,8 +90,8 @@ exports.actualizarTarea = async (req, res ) => {
         }
         // Crear un objeto con la nueva información
         const nuevaTarea = {};
-        if(nombre) nuevaTarea.nombre = nombre;
-        if(estado)  nuevaTarea.estado = estado;
+        nuevaTarea.nombre = nombre;
+        nuevaTarea.estado = estado;
 
         // Guardar la tarea
         tarea = await Tarea.findOneAndUpdate({_id : req.params.id }, nuevaTarea, { new: true } );
