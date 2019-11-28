cc12b6ae6809f4646b8101b7e9e04f3d6d1f199f
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-28 14:56:24 -0600

    subiendo conexión a la bd

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB Conectada');
+    } catch (error) {
+        console.log('hubo un error')
+        console.log(error);
+        process.exit(1); // Detener la app
+    }
+}
+
+module.exports = conectarDB;
